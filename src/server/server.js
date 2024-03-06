57c75db8f47221129348e8525c8d57c5ff241b51
Lopez <lopez_d77@hotmail.com>
2024-03-06 17:28:38 -0300

    pracitca de server con express.js

---
+++ src/server/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const path = require('path')
+
+const startServer = (options) =>{
+    const{port, public_path = 'public'} = options
+    const app = express()
+    //Para poder usar middlewares se usa .use (express) 
+    app.use(express.static(public_path))//contenido estatico que ponemos disponible
+    //recibir una peticion, por eso usamos  .get
+    //esto hace que siempre redirija al index.html
+    app.get('*', (req, res) => {
+        const indexPath = path.join(__dirname + `../../${public_path}/index.html`)
+        res.sendFile(indexPath)
+
+    })
+    //.listen abre un puerto y se queda escuchando
+    app.listen(port, () => {
+        console.log(`Escuchando en el puerto ${port}`)
+    })
+}
+
+module.exports = {
+    startServer
+}
